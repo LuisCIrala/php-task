bee29488b41ca4e9e4d5db916c5b07c6d2e3e749
LuisCIrala <luissesamo@hotmail.es>
2024-12-01 14:00:41 -0300

    Feature: Agregando los metodos DELETE  y PUT para borrar y actualizar tareas

--- api.php
+++ api.php
@@ -5,7 +5,7 @@ define('DB_FILE', 'tasks.json');
 // Funcion para Cargar las tareas desde el archivo JSON
 function loadTasks() {
     if (!file_exists(DB_FILE)) {
-        file_put_contents(DB_FILE, json_encode([])); // Crear el archivo si no existe
+        file_put_contents(DB_FILE, json_encode([])); // sirve para crear el archivo si no existe
     }
     return json_decode(file_get_contents(DB_FILE), true);
 }
@@ -58,45 +58,46 @@ if ($path === 'tasks') {
             }
             break;
 
-        case 'PUT': // Actualizar una tarea
-            $data = getRequestData();
-            if (isset($data['id'], $data['title'], $data['description'], $data['status'])) {
-                foreach ($tasks as &$task) {
-                    if ($task['id'] === $data['id']) {
-                        $task['title'] = $data['title'];
-                        $task['description'] = $data['description'];
-                        $task['status'] = $data['status'];
-                        saveTasks($tasks);
-                        respond($task);
+            case 'PUT': // Actualizar una tarea
+                $data = getRequestData();
+                if (isset($data['id'], $data['title'], $data['description'], $data['status'])) {
+                    foreach ($tasks as &$task) {
+                        if ($task['id'] === $data['id']) {
+                            $task['title'] = $data['title'];
+                            $task['description'] = $data['description'];
+                            $task['status'] = $data['status'];
+                            saveTasks($tasks);
+                            respond($task);
+                        }
                     }
-                }
-                respond(['error' => 'No se encontro la tarea!'], 404);
-            } else {
-                respond(['error' => 'Datos ingresados incorrectos'], 400);
-            }
-            break;
-
-        case 'DELETE': // Eliminar una tarea
-            $data = getRequestData();
-            if (isset($data['id'])) {
-                $found = false;
-                $tasks = array_filter($tasks, function ($task) use ($data, &$found) {
-                    if ($task['id'] === $data['id']) {
-                        $found = true;
-                        return false;
-                    }
-                    return true;
-                });
-                if ($found) {
-                    saveTasks($tasks);
-                    respond(['message' => 'Tarea eliminada!']);
+                    respond(['error' => 'Tarea no encontrada!'], 404);
                 } else {
-                    respond(['error' => 'No se encontro la tarea!'], 404);
+                    respond(['error' => 'datos ingresados incorrectos'], 400);
                 }
-            } else {
-                respond(['error' => 'Datos introducidos incorrectos'], 400);
-            }
-            break;
+                break;
+    
+            case 'DELETE': // Eliminar una tarea
+                $data = getRequestData();
+                if (isset($data['id'])) {
+                    $found = false;
+                    $tasks = array_filter($tasks, function ($task) use ($data, &$found) {
+                        if ($task['id'] === $data['id']) {
+                            $found = true;
+                            return false;
+                        }
+                        return true;
+                    });
+                    if ($found) {
+                        saveTasks($tasks);
+                        respond(['message' => 'Tarea eliminada']);
+                    } else {
+                        respond(['error' => 'Tarea no encontrada'], 404);
+                    }
+                } else {
+                    respond(['error' => 'Datos introducidos incorrectos!'], 400);
+                }
+                break;
+    
 
         default:
             respond(['error' => 'Metodo no permitido'], 405);
